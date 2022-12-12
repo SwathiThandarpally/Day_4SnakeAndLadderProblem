a4a705ea9ab28ef718b1457e926d30696500850c
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-12 15:30:13 +0530

    Added Program Of DiceRolls To Play

---
+++ UC_2DiceRolls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class UC_2DiceRolls
+    {
+        int position = 0;
+        public void RollDice()
+        {
+            Random random = new Random();
+            int position = random.Next(1, 7);
+            Console.WriteLine("The number of the Dice is:" +position);
+        }
+    }
+}
