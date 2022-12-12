511e385132bfaee355e664e20c739f12a159dd96
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-12 15:26:34 +0530

    Added Program Of WithOutPalyer Game

---
+++ UC_1WithOnePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public  class UC_1WithOnePlayer
+    {
+       
+        public void Position()
+        {
+            int position = 0;
+            Console.WriteLine("Game is started");
+
+        }
+    }
+}
